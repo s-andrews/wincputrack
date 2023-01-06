342f148efcbfeecc16201648304554a3f93647c1
Simon Andrews <simon.andrews@babraham.ac.uk>
2023-01-06 11:24:19 +0000

    Improve plot. Add back zero CPU measures

--- plot_cpu.R
+++ plot_cpu.R
@@ -13,7 +13,28 @@ data %>%
     ) %>%
     group_by(Epoch,Program) %>%
     summarise(CPU=sum(CPU)) %>%
-    ungroup() %>%
+    ungroup() -> data
+
+# Because we don't record processes with zero CPU they may be missing
+# from some epochs.  We need to add in a zero to any observed epoch where
+# a given process is not seen.
+
+data %>%
+  pivot_wider(
+    names_from=Program,
+    values_from=CPU
+  ) %>%
+  pivot_longer(
+    cols=-Epoch,
+    names_to="Program",
+    values_to="CPU"
+  ) %>%
+  mutate(
+    CPU=replace_na(CPU,0)
+  ) -> data
+
+
+data %>%
     mutate(
         date=as_datetime(Epoch)
     ) -> data
@@ -30,10 +51,11 @@ data %>%
 
 data %>%
   group_by(Program) %>%
-  filter(max(CPU) > 100) %>%
+  filter(max(CPU) > 90) %>%
   ungroup() %>%
   ggplot(aes(x=date,y=CPU, colour=Program)) +
-  geom_line(size=2) -> plot
+  geom_line(size=1) +
+  geom_point(size=2) -> plot
 
 ggsave(svg_file, plot=plot, device = "svg", width = 11, height = 4, units = "in")
 
